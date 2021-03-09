2badcfcb57900bfadc71f714cf5666b6036a1bd3
Enamul Kabir <enamul.kabirseu@gmail.com>
2021-03-09 14:39:05 +0600

    ui design

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,56 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
-import uuid from 'uuidv4'
-import api from './api/contacts'
-import Header from './Header'
-import AddContact from './AddContact'
-import ContactList from './ContactList'
-import ContactDetails from './ContactDetails'
-import contacts from './api/contacts';
-
 
 function App() {
-    const LOCAL_STORAGE_KEY = "contacts"
-    const [contact, setContacts] = useState([]);
-
-    // Retrieve Contacts
-    const retrieveContacts = async () => {
-        const response = await api.get("/contacts");
-        return response.data;
-        console.log(response);
-    }
-
-    // Add Contact
-    const addContactHandler = (contact) => {
-      console.log(contact);
-      setContacts([...contacts, {id: uuid(), ...contact}]);
-    }
-
-    // Remove Contact
-    const removeContactHandler = (id) => {
-      const newContactList = contacts.filter((contact) => {
-        return contact.id !== id;
-      })
-
-      setContacts(newContactList);
-    }
-
-    useEffect(() => {
-
-    }, []);
     
-
     return (
       <div className="App">
-          <Router>
-              <Header />
-              <Switch>
-                <Route exact path="/" component={ContactList} />
-                <Route exact path="/add" component={AddContact} />
-                <Route exact path="/details/:id" component={ContactDetails} />
-              </Switch>
-          </Router>
+          <h1>Welcome</h1>
       </div>
     );
 }
